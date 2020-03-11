6107399dec5c941402509bd46c99faf1f180d16b
Alexandre Belloni Alves <alexbelloni@outlook.com>
2020-03-11 08:24:14 -0400

    sendWebsites method

--- scripts/nasa.js
+++ scripts/nasa.js
@@ -1,41 +1,68 @@
 module.exports = (robot) => {
-  robot.hear("/API/i", (res) => {
-    res.send(`${res.message.text}? I should know about APIs...`);
+  robot.hear(/API/i, (res) => {
+    res.send(`${res.message.text}? I'm working to be an expert in NASA APIs. Reply me "websites", for instance.`);
     return
   })
 
-  const famousMessages = ['"Houston, we have a problem"', '"That is one small step for man, one giant leap for mankind."']
-  robot.respond("/famous message/i", (res) => {
-    res.send(res.random(famousMessages));
-    return
-  })
-
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+  robot.respond(/websites/i, (res) => sendWebsites(res, 2));
 
   const _robot = robot
-  robot.respond("/picture of the day/i", (res) =>  getThePictureOfTheDay(res, _robot, new Date()) )
+  robot.respond(/picture of the day/i, (res) => sendThePictureOfTheDay(res, _robot, new Date()));
 }
 
-function getThePictureOfTheDay(res, robot, day, lastAttempt) {
-    const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`
-    robot.http(url)
-      .header('accept', 'application/json')
-      .get()(function (err, resp, body) {
-        if (err) {
-          res.reply("error requesting picture data")
-          return
-        }
-        const data = JSON.parse(body);
-        if(data.code && !lastAttempt){ // picture is not ready today -> {"code":500,"msg":"Internal Service Error","service_version":"v1"}
-          const yesterday = new Date(day.setDate(day.getDate() -1));
-          getThePictureOfTheDay(res, robot, yesterday, true);
-          return
-        }
-        const info = !data ? 'no data' : data.title ? '' : url.concat(' ',body);
-        res.reply(`"${data.title}" by ${data.copyright}: ${data.url} ${info}`);
-      })
+function sendWebsites(res, quantity){
+  const nasaWebsites = [
+    "data.nasa.gov",
+    "api.nasa.gov",
+    "code.nasa.gov",
+    "open.nasa.gov",
+    "nasa.gov",
+    "images.nasa.gov",
+    "earthdata.nasa.gov",
+    "pds.nasa.gov",
+    "curator.jsc.nasa.gov",
+    "power.larc.nasa.gov",
+    "neo.sci.gsfc.nasa.gov",
+    "smap.jpl.nasa.gov",
+    "sti.nasa.gov",
+    "technology.nasa.gov",
+    "earthexplorer.usgs.gov",
+    "nhc.noaa.gov/satellite",
+    "usgs.gov",
+    "ncdc.noaa.gov",
+    "nodis3.gsfc.nasa.gov",
+    "ntrs.nasa.gov",
+    "resource-telescope.netlify.com",
+    "soundcloud.com/nasa",
+    "observablehq collection",
+    "blockbuilder nasa"]
+    const choices = [];
+    for(let i=0; i<(quantity <= nasaWebsites.length ? quantity : nasaWebsites.length); ++i ){
+      choices.push(res.random(nasaWebsites));
+    }
+    res.send(`My two suggestions: ${choices.join(", ")}`);
+    return
+}
+
+function sendThePictureOfTheDay(res, robot, day, lastAttempt) {
+  console.log(`sendThePictureOfTheDay ${res.message.text}`);
+  const url = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`
+  robot.http(url)
+    .header('accept', 'application/json')
+    .get()(function (err, resp, body) {
+      if (err) {
+        res.reply("error requesting picture data")
+        return
+      }
+      const data = JSON.parse(body);
+      if (data.code && !lastAttempt) {
+        const yesterday = new Date(day.setDate(day.getDate() - 1));
+        getThePictureOfTheDay(res, robot, yesterday, true);
+        return
+      }
+      const info = !data ? 'no data' : data.title ? '' : url.concat(' ', body);
+      res.reply(`"${data.title}" ${data.copyright ? "by ".concat(data.copyright) : "" }: ${data.url} ${info}`);
+    })
 }
 
 
